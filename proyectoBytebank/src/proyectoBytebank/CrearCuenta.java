3d901eecca742e9feb8f12e71731df0bea4ae71b
Neyberson <neyberatencio@gmail.com>
2023-09-14 12:53:12 -0500

    Add the PruebaMetodos and TestReferencia files, and the methods

--- proyectoBytebank/src/proyectoBytebank/CrearCuenta.java
+++ proyectoBytebank/src/proyectoBytebank/CrearCuenta.java
@@ -2,9 +2,22 @@ package proyectoBytebank;
 
 public class CrearCuenta {
 	public static void main(String[] args) {
+		// Variable          =  Valor
 		Cuenta primeraCuenta = new Cuenta();
-		primeraCuenta.saldo = 1000;
+		primeraCuenta.saldo = 400;
 		// primeraCuenta.pais = "Colombia"
 		System.out.println(primeraCuenta.saldo);
+		
+		Cuenta segundaCuenta = new Cuenta();
+		segundaCuenta.saldo = 400;
+		System.out.println(segundaCuenta.saldo);
+		//System.out.println(primeraCuenta.agencia);
+		System.out.println(primeraCuenta);
+		System.out.println(segundaCuenta);
+		if (primeraCuenta == segundaCuenta) {
+			System.out.println("Son el mismo objeto");
+		} else {
+			System.out.println("Son diferentes");
+		}
 	}
 }
--- proyectoBytebank/src/proyectoBytebank/Cuenta.java
+++ proyectoBytebank/src/proyectoBytebank/Cuenta.java
@@ -5,5 +5,18 @@ class Cuenta{
 	int agencia;
 	int numero;
 	String titular;
+	
+	public void depositar(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+	
+	public boolean retirar(double valor){
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 	
